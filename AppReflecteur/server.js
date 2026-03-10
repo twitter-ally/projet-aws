18850a6c9b436cf022358a6a3f4e23c367c80c10
napfenyy <>
2026-03-10 16:41:30 +0100

    ajout packages + ajout lien entre client.css et client.js + route '/' qui pointe vers index.html

--- AppReflecteur/server.js
+++ AppReflecteur/server.js
@@ -1,5 +1,12 @@
 var express = require('express');
+const bodyP = require('body-parser');
 var app = express();
 
+app.use(bodyP.json());
+app.use(express.static('pub'));
+
+app.all('/', (req, res) => {
+  res.sendFile(__dirname + '/pub/index.html');
+}); 
 
 app.listen(3000);
